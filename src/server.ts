da6400b6c6b4a3fb596c5e1a5fb7693f87acadbe
aranha111 <ikazuna19.10@gmail.com>
2025-12-15 22:29:32 -0300

    fix: usar env do Render em produção

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,10 @@ import { productsRoutes } from "./routes/products";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-dotenv.config();
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
+
 
 const app = Fastify();
 
